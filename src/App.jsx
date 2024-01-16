3bcfdb75e86e29a93bf1d02d321a0f299a1faf3b
ashishmohanty10 <ashishmohaty10@gmail.com>
2024-01-17 01:21:06 +0530

    add-proper-routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import { Home } from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
 import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Products from "./pages/Product";
+import Support from "./pages/Support";
+
 const App = () => {
   return (
     <>
@@ -11,6 +15,9 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/support" element={<Support />} />
         </Routes>
       </Router>
     </>
--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Pricing = () => {
-  return <div></div>;
+  return <div>Pricing</div>;
 };
 
 export default Pricing;
