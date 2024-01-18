14841c29837e214ecf82e2f4d405cf85f7e257ee
ashishmohanty10 <ashishmohaty10@gmail.com>
2024-01-18 23:28:18 +0530

    started with about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ThemeContext } from "../context/ThemeProvider";
 
 const About = () => {
-  return <div id="about">ABOUT</div>;
+  const { darkTheme, toggleDarkTheme } = useContext(ThemeContext);
+  return (
+    <div className={darkTheme ? "dark" : ""}>
+      <div className="bg-slate-800">
+        <div className="container">
+          <h3>
+            We pioneered the discount broking model in India. Now, we are
+            breaking ground with our technology.
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
