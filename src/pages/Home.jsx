6d363c80f6a1853101fddf251ef9620ed4df0eb5
ashishmohanty10 <ashishmohaty10@gmail.com>
2024-01-18 23:23:15 +0530

    added darkmode in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,8 +19,10 @@ export const Home = () => {
           <div className="flex flex-col justify-center items-center py-16 ">
             <img src={Landing} alt="" className="w-[1000px] mb-4" />
 
-            <h1 className="text-4xl font-medium mb-4">Invest in everything</h1>
-            <p className="text-xl font-normal mb-5">
+            <h1 className="text-4xl font-medium mb-4 dark:text-slate-100">
+              Invest in everything
+            </h1>
+            <p className="text-xl font-normal mb-5 dark:text-slate-300">
               Online platform to invest in stocks, derivatives, mutual funds,
               and more
             </p>
@@ -35,23 +37,23 @@ export const Home = () => {
             </div>
 
             <div>
-              <h3 className="text-3xl font-medium mb-5">
+              <h3 className="text-3xl font-medium mb-5 dark:text-slate-100">
                 Largest stock broker in India
               </h3>
 
-              <p className="text-base font-normal mb-4">
+              <p className="text-base font-normal mb-4 dark:text-slate-300">
                 1.3+ Crore Zerodha clients contribute to over 15% of all retail
                 order volumes in India daily by trading and investing in:
               </p>
 
               <div className="flex justify-between items-center mb-4">
-                <div className="text-base font-normal">
+                <div className="text-base font-normal dark:text-slate-300">
                   <li>Futures and Options</li>
                   <li>Commodity derivatives</li>
                   <li>Currency derivatives</li>
                 </div>
 
-                <div className="text-base font-normal">
+                <div className="text-base font-normal dark:text-slate-300">
                   <li>Stocks & IPOs</li>
                   <li>Direct mutual funds</li>
                   <li>Bonds and Govt. Securities</li>
@@ -66,21 +68,25 @@ export const Home = () => {
           {/* Third Section */}
           <div className="grid grid-cols-2 justify-between items-center gap-40 py-16">
             <div>
-              <h3 className="text-3xl font-medium mb-5">
+              <h3 className="text-3xl font-medium mb-5 dark:text-slate-100">
                 Trust with confidence
               </h3>
 
               <div className="mb-8">
-                <h4 className="text-xl font-medium">Customer-first always</h4>
-                <p className="mt-2 text-base font-normal">
+                <h4 className="text-xl font-medium dark:text-slate-100">
+                  Customer-first always
+                </h4>
+                <p className="mt-2 text-base font-normal dark:text-slate-300">
                   That's why 1.3+ crore customers trust Zerodha with ₹3.5+ lakh
                   crores worth of equity investments.
                 </p>
               </div>
 
               <div className="mb-8">
-                <h4 className="text-xl font-medium">No spam or gimmicks</h4>
-                <p className="mt-2 text-base font-normal">
+                <h4 className="text-xl font-medium dark:text-slate-100">
+                  No spam or gimmicks
+                </h4>
+                <p className="mt-2 text-base font-normal dark:text-slate-300">
                   No gimmicks, spam, "gamification", or annoying push
                   notifications. High quality apps that you use at your pace,
                   the way you like.
@@ -88,8 +94,10 @@ export const Home = () => {
               </div>
 
               <div className="mb-8">
-                <h4 className="text-xl font-medium">The Zerodha universe</h4>
-                <p className="mt-2 text-base font-normal">
+                <h4 className="text-xl font-medium dark:text-slate-100">
+                  The Zerodha universe
+                </h4>
+                <p className="mt-2 text-base font-normal dark:text-slate-300">
                   Not just an app, but a whole ecosystem. Our investments in 30+
                   fintech startups offer you tailored services specific to your
                   needs.
@@ -97,8 +105,10 @@ export const Home = () => {
               </div>
 
               <div>
-                <h4 className="text-xl font-medium">Do better with money</h4>
-                <p className="mt-2 text-base font-normal">
+                <h4 className="text-xl font-medium dark:text-slate-100">
+                  Do better with money
+                </h4>
+                <p className="mt-2 text-base font-normal dark:text-slate-300">
                   With initiatives like Nudge and Kill Switch, we don't just
                   facilitate transactions, but actively help you do better with
                   your money.
@@ -131,8 +141,10 @@ export const Home = () => {
           {/* Forth Section */}
           <div className="grid grid-cols-2 justify-between items-center gap-40 py-16">
             <div>
-              <h3 className="text-3xl font-medium mb-5">Unbeatable pricing</h3>
-              <p className="text-base font-normal mb-5">
+              <h3 className="text-3xl font-medium mb-5 dark:text-slate-100">
+                Unbeatable pricing
+              </h3>
+              <p className="text-base font-normal mb-5 dark:text-slate-300">
                 We pioneered the concept of discount broking and price
                 transparency in India. Flat fees and no hidden charges.
               </p>
@@ -148,15 +160,19 @@ export const Home = () => {
 
             <div className="grid grid-cols-2 justify-center items-center">
               <div className="flex flex-col justify-center items-center border border-slate-300 p-7 w-[192px]">
-                <p className="text-7xl font-semibold">₹0</p>
-                <p className="text-center">
+                <p className="text-7xl font-semibold dark:text-slate-300">₹0</p>
+                <p className="text-center dark:text-slate-300">
                   Free equity delivery and direct mutual funds
                 </p>
               </div>
 
               <div className="flex flex-col justify-center items-center border border-slate-300 p-7 w-[192px]">
-                <p className="text-7xl font-semibold">₹20</p>
-                <p>Intraday and F&O</p>
+                <p className="text-7xl font-semibold dark:text-slate-300">
+                  ₹20
+                </p>
+                <p className="dark:text-slate-300 text-center">
+                  Intraday and F&O
+                </p>
               </div>
             </div>
           </div>
@@ -168,11 +184,11 @@ export const Home = () => {
             </div>
 
             <div>
-              <h3 className="text-3xl font-medium mb-5">
+              <h3 className="text-3xl font-medium mb-5 dark:text-slate-100">
                 Free and open market education
               </h3>
 
-              <p className="text-base font-normal mb-4">
+              <p className="text-base font-normal mb-4 dark:text-slate-300">
                 Varsity, the largest online stock market education book in the
                 world covering everything from the basics to advanced trading.
               </p>
@@ -185,7 +201,7 @@ export const Home = () => {
                 <FontAwesomeIcon icon={faArrowRight} size="xs" />
               </div> */}
 
-              <p className="text-base font-normal mb-4 mt-4">
+              <p className="text-base font-normal mb-4 mt-4 dark:text-slate-300">
                 TradingQ&A, the most active trading and investment community in
                 India for all your market related queries.
               </p>
@@ -204,10 +220,10 @@ export const Home = () => {
           {/* Sicth Section */}
 
           <div className="flex justify-center items-center flex-col py-16">
-            <h1 className="text-4xl font-medium mb-4">
+            <h1 className="text-4xl font-medium mb-4 dark:text-slate-100">
               Open a Zerodha account
             </h1>
-            <p className="text-xl font-normal mb-5">
+            <p className="text-xl font-normal mb-5 dark:text-slate-300">
               Modern platforms and apps, ₹0 investments, and flat ₹20 intraday
               and F&O trades.
             </p>
