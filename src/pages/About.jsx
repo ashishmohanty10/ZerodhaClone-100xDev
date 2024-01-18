6aa7132cc60dccb68ba169e2347e8617a94bc745
ashishmohanty10 <ashishmohaty10@gmail.com>
2024-01-18 23:33:19 +0530

    about first section done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,10 +7,12 @@ const About = () => {
     <div className={darkTheme ? "dark" : ""}>
       <div className="bg-slate-800">
         <div className="container">
-          <h3>
-            We pioneered the discount broking model in India. Now, we are
-            breaking ground with our technology.
-          </h3>
+          <div className="flex justify-center items-center py-16 border-b border-slate-400 dark:border-primaryColor">
+            <h3 className="text-3xl font-medium dark:text-slate-100 text-center w-[60%]">
+              We pioneered the discount broking model in India. Now, we are
+              breaking ground with our technology.
+            </h3>
+          </div>
         </div>
       </div>
     </div>
