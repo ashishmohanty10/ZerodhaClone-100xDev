660137238e9784159cb1147d681d124b8a911e8a
ashishmohanty10 <ashishmohaty10@gmail.com>
2024-01-18 23:40:21 +0530

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,13 +5,15 @@ const About = () => {
   const { darkTheme, toggleDarkTheme } = useContext(ThemeContext);
   return (
     <div className={darkTheme ? "dark" : ""}>
-      <div className="bg-slate-800">
+      <div className="dark:bg-slate-800">
         <div className="container">
           <div className="flex justify-center items-center py-16 border-b border-slate-400 dark:border-primaryColor">
             <h3 className="text-3xl font-medium dark:text-slate-100 text-center w-[60%]">
               We pioneered the discount broking model in India. Now, we are
               breaking ground with our technology.
             </h3>
+
+            <h3>lorem1000</h3>
           </div>
         </div>
       </div>
