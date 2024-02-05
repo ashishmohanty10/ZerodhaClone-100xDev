eec453bb31e44e311152d94fb895746052a8b08d
ashishmohanty10 <ashishmohaty10@gmail.com>
2024-02-05 23:16:22 +0530

    top of footer section done

--- src/components/TopFooter.jsx
+++ src/components/TopFooter.jsx
@@ -16,8 +16,9 @@ const TopFooter = () => {
       <div className="dark:bg-slate-800">
         <div className="container">
           <div className="py-16">
-            <div className="grid grid-cols-4">
-              <div className="flex flex-col gap-4">
+            <section className="grid md:grid-cols-2 lg:grid-cols-4 justify-between items-start gap-5">
+              {/* first column */}
+              <div className="flex flex-col gap-y-4 mb-5 lg:mb-0">
                 <img src={Logo} alt="" className="w-[150px]" />
 
                 <p className="text-xs">
@@ -34,7 +35,89 @@ const TopFooter = () => {
                   <FaYoutube className="text-2xl text-slate-500 hover:text-rose-600 duration-75" />
                 </div>
               </div>
-            </div>
+
+              {/* second column */}
+              <div className="flex flex-col gap-y-4 mb-5 lg:mb-0">
+                <h3 className="text-lg font-medium text-slate-600">Company</h3>
+
+                <ul className="flex flex-col gap-y-2">
+                  <li className="text-base font-medium hover:text-primaryColor duration-75">
+                    <a href="">About</a>
+                  </li>
+                  <li className="text-base font-medium hover:text-primaryColor duration-75">
+                    <a href="">Prducts</a>
+                  </li>
+                  <li className="text-base font-medium text-slate-600 hover:text-primaryColor duration-75">
+                    <a href="">Pricing</a>
+                  </li>
+                  <li className="text-base font-medium text-slate-600 hover:text-primaryColor duration-75">
+                    <a href="">referral programme</a>
+                  </li>
+                  <li className="text-base font-medium text-slate-600 hover:text-primaryColor duration-75">
+                    <a href="">Careers</a>
+                  </li>
+                  <li className="text-base font-medium text-slate-600 hover:text-primaryColor duration-75">
+                    <a href="">Zerodha.tech</a>
+                  </li>
+                  <li className="text-base font-medium text-slate-600 hover:text-primaryColor duration-75">
+                    <a href="">Press & media</a>
+                  </li>
+                  <li className="text-base font-medium text-slate-600 hover:text-primaryColor duration-75">
+                    <a href="">Zerodha Xares (CSR)</a>
+                  </li>
+                </ul>
+              </div>
+
+              {/* third column */}
+              <div className="flex flex-col gap-y-4 mb-5 lg:mb-0">
+                <h3 className="text-lg font-medium text-slate-600">Support</h3>
+
+                <ul className="flex flex-col gap-y-2">
+                  <li className="text-base font-medium text-slate-600 hover:text-primaryColor duration-75">
+                    <a href="">Contact us</a>
+                  </li>
+                  <li className="text-base font-medium text-slate-600 hover:text-primaryColor duration-75">
+                    <a href="">Support portal</a>
+                  </li>
+                  <li className="text-base font-medium text-slate-600 hover:text-primaryColor duration-75">
+                    <a href="">Z-Connect blog</a>
+                  </li>
+                  <li className="text-base font-medium text-slate-600 hover:text-primaryColor duration-75">
+                    <a href="">List of Charges</a>
+                  </li>
+                  <li className="text-base font-medium text-slate-600 hover:text-primaryColor duration-75">
+                    <a href="">Download & resources</a>
+                  </li>
+                  <li className="text-base font-medium text-slate-600 hover:text-primaryColor duration-75">
+                    <a href="">Videos</a>
+                  </li>
+                  <li className="text-base font-medium text-slate-600 hover:text-primaryColor duration-75">
+                    <a href="">Market overview</a>
+                  </li>
+                  <li className="text-base font-medium text-slate-600 hover:text-primaryColor duration-75">
+                    <a href="">How to file a complaint?</a>
+                  </li>
+
+                  <li className="text-base  font-medium text-slate-600 hover:text-primaryColor duration-75">
+                    <a href="">Status of your complaints</a>
+                  </li>
+                </ul>
+              </div>
+
+              {/* Fourth column */}
+              <div className="flex flex-col gap-y-4 mb-5 lg:mb-0">
+                <h3 className="text-lg font-medium text-slate-600">Company</h3>
+
+                <ul className="flex flex-col gap-y-2">
+                  <li className="text-base font-medium text-slate-600 hover:text-primaryColor duration-75">
+                    <a href="">Open an account</a>
+                  </li>
+                  <li className="text-base font-medium text-slate-600 hover:text-primaryColor duration-75">
+                    <a href="">Fund Transfer</a>
+                  </li>
+                </ul>
+              </div>
+            </section>
           </div>
         </div>
       </div>
